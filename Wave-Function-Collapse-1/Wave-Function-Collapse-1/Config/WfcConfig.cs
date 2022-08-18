05fe06055dddc1966455f98aa87c6b58d0d3aa9b
Ferrero <leonardo.ferreromerlino@mooney.it>
2022-08-19 01:11:34 +0200

    Created the algorithm

---
+++ Wave-Function-Collapse-1/Wave-Function-Collapse-1/Config/WfcConfig.cs
@@ -0,0 +1,46 @@
+﻿
+
+using System.ComponentModel;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+
+namespace Wave_Function_Collapse_1.Config;
+
+public class WfcConfig
+{
+    public WfcConfig(string configJsonPath)
+    {
+        var configData = JsonConvert.DeserializeObject<JsonConfigStructure>(File.ReadAllText(configJsonPath));
+        if (configData is null) throw new NullReferenceException("The JSON config not founded");
+        CharsSet = configData.CharsSet;
+        CharsRule = configData.CharsRule;
+        ShowSteps = configData.ShowSteps;
+        StepsSpeed = configData.StepsSpeed;
+    }
+    
+    public readonly char[] CharsSet;
+    
+    public readonly List<Wfc.Tile> CharsRule;
+    
+    public readonly (int, int) GridSize = (30, 100);
+    
+    public readonly bool ShowSteps = false;
+    
+    public readonly int StepsSpeed = 100;
+
+    internal class JsonConfigStructure
+    {
+        [JsonPropertyName("CharsSet")]
+        public char[] CharsSet;
+    
+        [JsonPropertyName("CharsRule")]
+        public List<Wfc.Tile> CharsRule;
+        
+        [JsonPropertyName("ShowSteps")]
+        public bool ShowSteps;
+        
+        [JsonPropertyName("StepsSpeed")]
+        [Description("Time in ms")]
+        public int StepsSpeed;
+    }
+}
---
+++ Wave-Function-Collapse-1/Wave-Function-Collapse-1/Program.cs
@@ -0,0 +1,18 @@
+﻿using Wave_Function_Collapse_1.Config;
+
+namespace Wave_Function_Collapse_1;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        Wfc wfc = new(new WfcConfig(@"D:\Projects\Wave-Function-Collapse\Wave-Function-Collapse-1\Wave-Function-Collapse-1\Config\FirstCharSet.json"));
+        char regenerate;
+        do
+        {
+            wfc.Run();
+            Console.WriteLine("Regenerate? [y / n]");
+            regenerate = Console.ReadKey().KeyChar;
+        } while (regenerate == 'y');
+    }
+}
